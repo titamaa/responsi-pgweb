ed26cedb6ca60b70cc8a133bda7ec7b0e9e19432
titamaa <titaamaliasudarma@mail.ugm.ac.id>
2023-11-23 18:19:17 -0800

    add data

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "datars";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM slemancov"; 
+$result = $conn->query($sql);
+
+$data = array(); 
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row; 
+    }
+}
+
+$conn->close();
+
+
+echo json_encode($data);
+?>
